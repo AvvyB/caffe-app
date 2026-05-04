0081d9f2ff3047d4fc99c53907abc21bbfaa56ac
AvvyB <avdon.bergman@gmail.com>
2026-05-03 22:12:23 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,13 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, doc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE.appspot.com",
-  messagingSenderId: "PASTE",
-  appId: "PASTE"
+  apiKey: "AIzaSyDjvJTGisS42A4oDX7O5OyBo80Oezo3DSA",
+  authDomain: "espresso-ordering.firebaseapp.com",
+  projectId: "espresso-ordering",
+  storageBucket: "espresso-ordering.firebasestorage.app",
+  messagingSenderId: "375255695312",
+  appId: "1:375255695312:web:c887e58f002a6d68ca83d5",
+  measurementId: "G-EYT0J8T6C5"
 };
 
 const app = initializeApp(firebaseConfig);
